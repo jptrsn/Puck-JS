e1f589db87a7ba4f72670c78415e6cf9587fd504
jptrsn <ptrsnja@gmail.com>
2018-01-13 12:48:55 -0500

    Added documentation to readme.js

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Puck-JS
+# Puck JS Modules
+By James Petersen
+Published under an MIT License
+
+## Files
+
+### media_control.js
+This is a Bluetooth HID media controller, with play/pause, skip forward, skip backward, volume up, and volume down commands. To use, program your [Puck JS](https://www.puck-js.com/) with the contents of the media_control.js file.
+
+You will need to disconnect and un-pair your Puck in order to use the media controls, as the available services will have changed. Recommended use is paired with a phone using either a media player app (music playback or video) or a remote control app.
+
+The operations are as follows:
+
+##### *When Connected*
+
+- **play/pause**: a short click (less than 0.3 ms). Accompanied by a flash of the Green LED.
+
+- **skip forward**: a longer click (> 0.3 ms, and < 2s) with the Puck sitting right-side-up. Accompanied by a flash of the Blue LED.
+
+- **skip backward**: a longer click (> 0.3 ms, and < 2s) with the Puck sitting up-side-down. Accompanied by a flash of the Blue LED.
+
+- **volume up**: start with the Puck up-side-down. Click and hold, and turn the Puck right-side-up. Volume up commands will repeat every 250 ms until you release the button. Each volume up command is accompanied by a flash of the Green LED.
+
+- **volume down**: start with the Puck right-side-up. Click and hold, and turn the Puck up-side-down. Volume down commands will repeat every 250 ms until you release the button. Each volume up command is accompanied by a flash of the Red LED.
+
+- **disconnect**: press and hold the button for more than 2 seconds, keeping the Puck right-side-up. Sending the disconnect command is accompanied by a flash of both Blue and Red LEDs. When successfully disconnected, the Puck will flash the Red LED once.
+
+##### *When Disconnected*
+
+- **connect to the last device used**: any button press shorter than 2 seconds. The Puck show the Red and Green LEDs, and then flash Green if reconnected, or Red if the connection failed.
+
+- **connect to the specified device ID**: press the button for more than 2 seconds to attempt a connection with the device corresponding to the ID specified as `knownDeviceId` in media_control.js file.
--- media_control.js
+++ media_control.js
@@ -1,100 +1,98 @@
 var controls = require("ble_hid_controls");
-
 NRF.setServices(undefined, { hid : controls.report });
-var isSleeping = true;
-var connected = true;
-var toggleSleeping;
-var lastDeviceId;
-var orientationMidpoint = -1500;
-var phoneId = "48:59:29:05:b5:38";
-var pcId = "00:1a:7d:da:71:11";
-var lastErr, risingOrientation, fallingOrientation;
 
-NRF.on('connect', function(id) {
-  lastDeviceId = id.split(" ")[0];
+var connected = true;
+var lastDeviceId, lastErr, risingOrientation;
+var orientationMidpoint = -1500;
+var knownDeviceId = "00:1a:7d:da:71:11";
+
+NRF.on("connect", function (id) {lastDeviceId = id.split(" ")[0];
   connected = true;
-  //NRF.setLowPowerConnection(true);
+  NRF.setLowPowerConnection(true);
   blinkLeds(2,1500);
 });
 
-NRF.on('disconnect', function() {
-  blinkLeds(1,1500);
+NRF.on("disconnect", function () {
   connected = false;
+  blinkLeds(1,1500);
 });
 
-var activeButtonHandler = function(e) {
-  try {
-    console.log('activeButtonHandler');
+
+function activeButtonHandler(e) {try {
     var len = e.time - e.lastTime;
+    console.log('activeButtonHandler', len);
     var orientationDelta = getOrientationDelta();
-   
-    if (len > 0.2 && len < 2) {
-      if (orientationDelta) {
-        if (orientationDelta > 0) {
-          console.log('volume up');
-          controls.volumeUp();
-        } else {
-          console.log('volume down');
-          controls.volumeDown();
-        }
+    if (!orientationDelta && len > 0.3 && len < 2) {
+      if (getStaticOrientation()) {
+        console.log('next');
+        controls.next();
+        blinkLeds(5,100);
       } else {
-        if (getStaticOrientation()) { 
-          console.log('next');
-          controls.next();
-        } else {
-          console.log('previous');
-          controls.prev();
-        }
+        console.log('previous');
+        controls.prev();
+        blinkLeds(5,100);
       }
-      
-      blinkLeds(3,100);
     } else if (len <= 0.3) {
       console.log('playpause');
       controls.playpause();
       blinkLeds(2,100);
-    } else {
-      console.log('going to sleep');
-      //blinkLeds(4,1000, toggleSleeping);
+    } else if (!orientationDelta) {
+      console.log('disconnecting');
+      NRF.disconnect(function(reason){
+        blinkLeds(4,1000);
+      });
     }
   } catch(err) {
       handle(err);
   }
-};
+}
 
-function handle(e) {
-  blinkLeds(1,150);
-  console.log(e.msg);
+function handle(e) {blinkLeds(1,150);
   lastErr = e;
+  console.log(e.msg);
 }
 
-function lastError() {
-  console.log(lastErr);
+function lastError() {console.log(lastErr);}
+
+function checkVolumeCommand() {
+  var delta = getOrientationDelta();
+  console.log('delta',delta);
+  if (delta) {
+    var btn = digitalRead(BTN);
+    console.log(btn);
+    if (btn) {
+      if (delta > 0) {
+        controls.volumeUp();
+        blinkLeds(2,100);
+      } else if (delta < 0) {
+        controls.volumeDown();
+        blinkLeds(1,100);
+      }
+      setTimeout(checkVolumeCommand,250);
+    }
+  }
 }
 
-var wakeupButtonHandler = function(e) {
-  try {
+function wakeupButtonHandler(e) {try {
     console.log('wakeupButtonHandler');
-    //NRF.wake();
     var len = e.time - e.lastTime;
-    if (len > 2) {
+    if (len < 2) {
       setLeds(3);
       NRF.connect(lastDeviceId).then(function() {
         blinkLeds(2,1000);
       }, handle);
     } else {
       setLeds(5);
-      NRF.connect(pcId).then(function() {
+      NRF.connect(knownDeviceId).then(function() {
         blinkLeds(2,1000);
       }, handle);
     }
-    blinkLeds(7,150, toggleSleeping);
   } catch(err) {
     handle(err);
   }
-};
+}
 
-function blinkLeds(c, d, optCallback) {
-  setLeds(c);
+function blinkLeds(c,d,optCallback) {setLeds(c);
   setTimeout(function() {
     setLeds(0);
     if (optCallback) {
@@ -103,75 +101,34 @@ function blinkLeds(c, d, optCallback) {
   },d || 500);
 }
 
-function setLeds(color) {
-  digitalWrite([LED3,LED2,LED1], color);
-}
+function setLeds(color) {digitalWrite([LED3,LED2,LED1], color);}
 
-function connectTo(id) {
-  //NRF.wake();
-  setLeds(3);
+function connectTo(id) {setLeds(3);
   NRF.connect(id).then(function(ev){
     blinkLeds(2,500);
     setLeds(0);
     console.log(ev);
   }, function(ev) {
     blinkLeds(1,250);
-  });
-}
-
-toggleSleeping = function() {
-  if (isSleeping) {
-    //NRF.wake();
-    blinkLeds(6,500);
-  } else {
-    blinkLeds(1,150);
-    NRF.disconnect(function() {
-      //blinkLeds(1,5000, NRF.sleep);
-      blinkLeds(2,5000);
-    });
-  }
-  isSleeping = !isSleeping;
-};
+  });}
 
 function fallingEdgeClickHandler(e) {
   try {
     var len = e.time - e.lastTime;
-    
-    if (!connected) {
-      if (lastDeviceId) {
-        console.log('connecting to '+lastDeviceId);
-        setLeds(3);
-        connectTo(lastDeviceId);
-      } else {
-        
-        if (len < 2) {
-          setLeds(1);
-          console.log('connecting to pc');
-          connectTo(pcId);
-        } else {
-          console.log('ready state');
-          blinkLeds(6,500);
-        }
-      }
-      
-      return;
-    }
     blinkLeds(2,100);
-    if (isSleeping) {
-      console.log('waking up');
+    if (!connected) {
+      console.log('disconnected');
       wakeupButtonHandler(e);
     } else {
-      console.log('awake');
+      console.log('connected');
       activeButtonHandler(e);
     }
-    
   } catch(err) {
     blinkLeds(1,5000);
   }
 }
 
-function getOrientationDelta() {
-  var o = Puck.mag();
+function getOrientationDelta() {var o = Puck.mag();
   var dz = o.z-risingOrientation.z;
   if (dz < -500) {
     orientationMidpoint = (risingOrientation.z + o.z) / 2;
@@ -189,14 +146,10 @@ function getStaticOrientation() {
   return (o.z > orientationMidpoint);
 }
 
-function risingEdgeClickHandler(e) {
-  risingOrientation = Puck.mag();
-  //console.log(risingOrientation);
+function risingEdgeClickHandler(e) {risingOrientation = Puck.mag();
+  setTimeout(checkVolumeCommand,250);
 }
 
-blinkLeds(2,100);
-toggleSleeping();
+setWatch(fallingEdgeClickHandler, D0, { repeat:true, edge:'falling', debounce : 50 });
 
-setWatch(fallingEdgeClickHandler, BTN, { edge:"falling",repeat:true,debounce:50});
-
-setWatch(risingEdgeClickHandler, BTN, { edge:"rising",repeat:true,debounce:50});
+setWatch(risingEdgeClickHandler, D0, { repeat:true, edge:'rising', debounce : 50 });
